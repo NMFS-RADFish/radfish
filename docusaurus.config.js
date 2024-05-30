d5d71309dcb2e687374b4efb461f0ed25ce439e8
IsaiahHarris <isaiah.harris.hi@gmail.com>
2024-05-30 12:09:30 -1000

    broken warn

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   organizationName: "NMFS-RADFish", // Usually your GitHub org/user name.
   projectName: "documentation-new", // Usually your repo name.
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
