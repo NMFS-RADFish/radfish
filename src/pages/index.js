3092b65bf82dcc6152c3243158432be86f9ffd4d
Jay Giang <jayhgiang@gmail.com>
2024-07-31 21:51:34 -0700

    Remove cta button from homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,14 +17,6 @@ function HomepageHeader() {
           {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/category/getting-started"
-          >
-            RADFish Tutorial - 5min ⏱️
-          </Link>
-        </div>
       </div>
     </header>
   );
