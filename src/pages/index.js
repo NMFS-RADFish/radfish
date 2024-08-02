f88acceec17b126d6622c2dd63f3ab878f086f89
IsaiahHarris <isaiah.harris.hi@gmail.com>
2024-08-02 10:44:11 -1000

    space

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/category/getting-started"
+            to="/docs/getting-started"
           >
             RADFish Tutorial - 5min ⏱️
           </Link>
