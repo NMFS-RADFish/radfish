4c3620cee9fdfe2af5b366315809b6e768718d55
Jay Giang <jayhgiang@gmail.com>
2024-08-02 17:41:01 -0700

    Remove edit functionality

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,10 +40,6 @@ const config = {
       ({
         docs: {
           sidebarPath: "./sidebars.js",
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-        //   editUrl:
-        //     "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
           customCss: "./src/css/custom.css",
