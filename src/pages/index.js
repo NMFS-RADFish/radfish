6959560698ce3bf8634e3665ace19ab6eac7926a
Jay Giang <jayhgiang@gmail.com>
2024-08-05 14:43:47 -0700

    Add CTA button with updated link and text

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,6 +17,14 @@ function HomepageHeader() {
           {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/getting-started"
+          >
+            Get started with RADFish
+          </Link>
+        </div>
       </div>
     </header>
   );
