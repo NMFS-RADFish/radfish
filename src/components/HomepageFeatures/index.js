072ade5e5490f0a784f8ad69e90c2fb580a0d4d2
Joshua Lee <77109426+JLee2021@users.noreply.github.com>
2024-09-22 09:43:29 -0400

    adding a new section to docs home and wrapping the list into cards

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -2,10 +2,27 @@ import clsx from "clsx";
 import Heading from "@theme/Heading";
 import styles from "./styles.module.css";
 
+function RADFishHero() {
+  return (
+    <section className={clsx(styles.hero, "text--center")}>
+      <div className="container">
+        <div className="row">
+          <div className={clsx("col col--12 padding-horiz--md")}>
+            <h3 className={styles.heroTitle}>
+              Introducing the React Application Development Framework for
+              Fisheries (RADFish)
+            </h3>
+            <p className={styles.heroSubtitle}>text</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
 const FeatureList = [
   {
     title: "Easy to Use",
-    // Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
     description: (
       <>
         RADFish provides a simple standard for modern frontend web development
@@ -15,7 +32,6 @@ const FeatureList = [
   },
   {
     title: "Convenient Tooling",
-    // Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
     description: (
       <>
         RADFish enables common development tasks like on-device storage and
@@ -25,7 +41,6 @@ const FeatureList = [
   },
   {
     title: "Powered by React",
-    // Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
     description: (
       <>
         Extend or customize your website layout by reusing React examples.
@@ -38,9 +53,14 @@ const FeatureList = [
 function Feature({ title, description }) {
   return (
     <div className={clsx("col col--4")}>
-      <div className='text--center padding-horiz--md'>
-        <Heading as='h3'>{title}</Heading>
-        <p>{description}</p>
+      {/* Card container */}
+      <div className={styles.card}>
+        <div className="card__header">
+          <h3>{title}</h3>
+        </div>
+        <div className="card__body">
+          <p>{description}</p>
+        </div>
       </div>
     </div>
   );
@@ -48,14 +68,17 @@ function Feature({ title, description }) {
 
 export default function HomepageFeatures() {
   return (
-    <section className={styles.features}>
-      <div className='container'>
-        <div className='row'>
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
+    <>
+      <RADFishHero />
+      <section className={styles.features}>
+        <div className="container">
+          <div className="row">
+            {FeatureList.map((props, idx) => (
+              <Feature key={idx} {...props} />
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
